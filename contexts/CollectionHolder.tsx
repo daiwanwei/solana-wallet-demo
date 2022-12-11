475f2e3b6a1a00da3ce838aada89d91f10b390a9
daiwanwei <wadejet.work@gmail.com>
2022-12-12 05:08:27 +0800

    update fetch nft

--- contexts/CollectionHolder.tsx
+++ contexts/CollectionHolder.tsx
@@ -1,14 +1,16 @@
 import {createContext, FC, ReactNode, useContext, useEffect, useState} from "react";
 import {PublicKey} from "@solana/web3.js";
 import {useConnection, useWallet} from "@solana/wallet-adapter-react";
-import {getHeldToken, getTokenBalance} from "../helpers/token";
+import {getHeldToken} from "../helpers/token";
 import {useCollectionList} from "./CollectionList";
 
 export interface TokenInfo {
-    mint: string
-    name: string
-    symbol: string
+    creator: string
+    collectionName: string
+    tokenName: string
+    amount: number
     image: string
+    mint: string
 }
 
 export interface CollectionHolderContextState {
@@ -39,32 +41,23 @@ export const CollectionHolderProvider: FC<{children:ReactNode}> = (
 ) => {
     const {publicKey} = useWallet();
     const {connection} = useConnection();
-    const {collectionMap} = useCollectionList();
+    const {collectionList} = useCollectionList();
     const [isHolder, setIsHolder] = useState(false);
     const [tokens, setTokens] = useState<TokenInfo[]>([]);
 
     useEffect(() => {
         const update = async () => {
-            let ts: TokenInfo[] = []
             if (!publicKey) {
                 setIsHolder(false)
                 return
             }
-            let heldToken = await getHeldToken(connection, publicKey)
-            for (let token of heldToken) {
-
-                if (collectionMap.has(token.mint.toBase58())) {
-                    const info = collectionMap.get(token.mint.toBase58())
-                    if (info) ts.push(info)
-                }
-            }
-
-            if (ts.length > 0) {
+            let heldToken = await getHeldToken(publicKey,collectionList)
+            if (heldToken.length > 0) {
                 setIsHolder(true)
             } else {
                 setIsHolder(false)
             }
-            setTokens(ts)
+            setTokens(heldToken)
         }
         update().catch(e => alert(`get err with update :${e}`))
     }, [publicKey, connection])
